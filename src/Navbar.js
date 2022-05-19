292ba5b75c59258f3784e9462fe7ebeca60773bc
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-19 12:53:40 +0530

    navbar getBoundingClientRect for dynamic height

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,19 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  //div ref
+  const linksContainerRef = useRef(null);
+  //ul list ref
+  const linksRef = useRef(null);
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
 
   return (
     <nav>
@@ -18,12 +31,8 @@ const Navbar = () => {
             <Hamburger size={20} />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
