d5f1176b5fe844f2040f6326b752f1640fea9fb4
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-26 19:47:47 +0530

    created categories comp

--- src/Menu/Index.jsx
+++ src/Menu/Index.jsx
@@ -7,6 +7,11 @@ const Index = () => {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(items);
 
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu section">
@@ -14,7 +19,7 @@ const Index = () => {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Category />
+        <Category filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
