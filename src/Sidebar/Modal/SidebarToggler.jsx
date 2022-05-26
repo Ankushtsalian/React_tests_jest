944006ce95aac06f0b542d7818a33d13d15ea005
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-26 10:22:12 +0530

    created global context hook

--- src/Sidebar/Modal/SidebarToggler.jsx
+++ src/Sidebar/Modal/SidebarToggler.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "../../context/context";
 
 const SidebarToggle = () => {
+  const data = useGlobalContext();
+  console.log({ data });
   return (
     <main>
       <button className="sidebar-toggle">
