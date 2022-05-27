3ef0d9ef25f120b979169b66c1faa1d81bbca140
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-27 10:27:56 +0530

    created tab comp url fetch with loading

--- src/Tabs/Tabs.jsx
+++ src/Tabs/Tabs.jsx
@@ -14,7 +14,9 @@ const Tabs = () => {
     setLoading(false);
   };
 
-  useEffect(() => fetchJobs(), []);
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
   if (loading) {
     return <h1>Loading....</h1>;
