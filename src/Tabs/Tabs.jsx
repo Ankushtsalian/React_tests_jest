7d0cb11ac57fceaa1c8b04bc8c09e428034459db
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-27 10:17:54 +0530

    created tab comp url

--- src/Tabs/Tabs.jsx
+++ src/Tabs/Tabs.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import "./tabs.css";
+const url = "https://course-api.com/react-tabs-project";
 const Tabs = () => {
   return <div>Tabs</div>;
 };
