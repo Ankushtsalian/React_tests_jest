9fd630903e9b1862bf87335a6b53a86fff490312
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-29 13:00:48 +0530

    updated css

--- src/Stripe/Navbar.jsx
+++ src/Stripe/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./stripe.css";
 import logo from "./images/logo.svg";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "../context/context";
@@ -10,8 +11,26 @@ const Navbar = () => {
     <nav className="stripe-nav">
       <div className="stripe-nav-center">
         <div className="stripe-nav-header">
-          <img src={logo} alt="stripe" />
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button
+            className="stripe-btn stripe-toggle-btn"
+            onClick={openStripeSidebar}
+          >
+            <FaBars />
+          </button>
         </div>
+        <ul className="stripe-nav-links">
+          <li>
+            <button className="stripe-link-btn">products</button>
+          </li>
+          <li>
+            <button className="stripe-link-btn">developers</button>
+          </li>
+          <li>
+            <button className="stripe-link-btn">company</button>
+          </li>
+        </ul>
+        <button className="stripe-btn signin-btn">Sign In</button>
       </div>
     </nav>
   );
