19ef1f5e968e10cf494fa0ceb20a8b99e41e1995
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-01 19:39:06 +0530

    created hero comp phone image

--- src/Stripe/Hero.jsx
+++ src/Stripe/Hero.jsx
@@ -3,8 +3,25 @@ import { useGlobalContext } from "../context/context";
 import phoneImg from "./images/phone.svg";
 
 const Hero = () => {
-  const DATA = useGlobalContext();
-  return <h2>hero component</h2>;
+  const { closeStripeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="stripe-btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/Stripe/Submenu.jsx
+++ src/Stripe/Submenu.jsx
@@ -3,7 +3,7 @@ import { useGlobalContext } from "../context/context";
 
 const Submenu = () => {
   const data = useGlobalContext();
-  return <h2>submenu component</h2>;
+  return <h2>{/* submenu component */}</h2>;
 };
 
 export default Submenu;
