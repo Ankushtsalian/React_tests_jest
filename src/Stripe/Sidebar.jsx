9bec1dcb1fdad69b5a34f0dd2933a8297cd6c115
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-01 20:16:50 +0530

    created sidebar functionality map sublinks inside links

--- src/Stripe/Sidebar.jsx
+++ src/Stripe/Sidebar.jsx
@@ -18,6 +18,27 @@ const Sidebar = () => {
         <button className="stripe-close-btn" onClick={closeStripeSidebar}>
           <FaTimes />
         </button>
+        <div className="stripe-sidebar-links">
+          {sublinks.map((item, index) => {
+            const { links, page } = item;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="stripe-sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
